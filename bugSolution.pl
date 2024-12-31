7de761fd90a319ed61805ac2e48f03e9c777b648
BugHunter-X <askzenai@gmail.com>
2024-12-31 03:56:37 +0100

    Add bugSolution.pl file

---
+++ bugSolution.pl
@@ -0,0 +1,1 @@
+use strict; use warnings; use Sort::Naturally; my %hash = (a => 1, b => 2, c => 3); my @sortedKeys = sort { $a cmp $b } keys %hash; foreach my $key (@sortedKeys) { print "$key => $hash{$key}\n"; }
